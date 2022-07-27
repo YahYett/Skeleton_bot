d4cab13dbd60a5dee7a26b94a2f7d89a43badf05
YahYett <34634513+YahYett@users.noreply.github.com>
2022-07-26 21:46:27 -0400

    This is your basic command setup

--- commands/Basic/cmd.js
+++ commands/Basic/cmd.js
@@ -2,7 +2,7 @@
     Steps of making new commands:
     1. Create a new folder in the commands folder.
     2. Create a new file in the folder.
-    3. Use a 'module.export' to export the command (so you can call it in the main file using 'require').
+    3. Use a 'module.exports' to export the command (so you can call it in the main file using 'require').
     4. Make any needed object properties and methods.
     5. The 'Run' method can be named whatever you like, be sure to call it the same name in the main file.
     6. Code away!
